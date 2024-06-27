7d2e0a849232d599faf6f0995214efa626088cab
EliBrank <eliasbrinkman@gmail.com>
2024-06-27 18:43:02 +0000

    add comment to beginning of script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- create database hbnb_dev_db and user hbnb_dev
+-- hbnb_dev given all privileges on hbnb_dev_db, SELECT privilege on performance_schema
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY "hbnb_dev_pwd";
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
