55ac20cd176207051b41b8b3412f1e28d1079ea5
EliBrank <eliasbrinkman@gmail.com>
2024-06-27 19:13:42 +0000

    wrap names in single quotes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 -- hbnb_dev given all privileges on hbnb_dev_db, SELECT privilege on performance_schema
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY "hbnb_dev_pwd";
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
