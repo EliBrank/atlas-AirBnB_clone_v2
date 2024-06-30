278d55847232f5a977556ed07247830712f94c63
EliBrank <eliasbrinkman@gmail.com>
2024-06-30 16:46:57 +0000

    start working on city, state classes

--- models/city.py
+++ models/city.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
+from models import UsingStorage
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
 
 
-class City(BaseModel):
-    """ Represents a city with attributes """
-    if getenv('HBNB_TYPE_STROAGE') == 'db':
+class City(BaseModel, Base):
+    """ City class """
+    if UsingStorage.DB_STORAGE:
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states_id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
+        # places = relationship("Place", backref="cities", cascade="all, delete-orphan")
     else:
         state_id = ""
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models import UsingStorage
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if UsingStorage.DB_STORAGE:
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states_id'), nullable=False)
+        cities = relationship(
+            'City',
+            # names reference (within city back to State object) as state
+            backref='state',
+            # specifies behavior when the instance is deleted
+            # 'all' uses all cascade options (delete, delete-orphan, etc.)
+            cascade='all'
+        )
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """getter attribute for cities"""
+
+            from models import storage
+            from models.city import City
+
+            # initialize list to save cities
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                # check each city's state_id against those connected to State object
+                if city.state_id == self.id:
+                    city_list.append(city)
+
+            return city_list
