757d3ec7d7bd6d0570e39cc61451f7a3ba92288b
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-01 04:23:54 +0000

    Updated place and user to complete requests.

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" User Module for HBNB project """
+from models.base_model import BaseModel, Base
+import sqlalchemy
+import os
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """This class defines a user"""
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+        # backref creates a bidirectional relationship by
+        # automatically creating an attribute in the related
+        # class
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
