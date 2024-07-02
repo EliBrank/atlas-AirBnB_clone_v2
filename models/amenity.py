cd2db59b2d298c05df576ebfe532a5ff47cbec49
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-02 14:23:05 +0000

    built out amenity.

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,15 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models import UsingStorage
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models import UsingStorage
 
 
 class Amenity(BaseModel):
-    name = ""
+        
+    if UsingStorage.DB_STORAGE:
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
