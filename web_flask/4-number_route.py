a5193d5559c62d9d34b7f747b380b95f06fdce4d
EliBrank <eliasbrinkman@gmail.com>
2024-07-06 20:10:58 +0000

    add route for variable number with validation

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""starts simple Flask web application"""
+
+from flask import Flask, abort
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_var(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_var(text="is_cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<n>")
+def n_var(n):
+    if n.isdigit():
+        n = int(n)
+        return "{} is a number".format(n)
+    else:
+        abort(400, description="n must be a number")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
