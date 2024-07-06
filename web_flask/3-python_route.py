9ae502a41a8c3361d42fbf3db76fb68ea8d41b51
EliBrank <eliasbrinkman@gmail.com>
2024-07-06 19:47:40 +0000

    add extra route for python with default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""starts simple Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_var(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_var(text="is_cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
